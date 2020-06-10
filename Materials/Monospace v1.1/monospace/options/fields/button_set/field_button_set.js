1cff5f2b878630ee4547356842496d1255e14238
TruHee <trunghieult1807@gmail.com>
2020-06-10 13:05:32 +0700

    new

--- Materials/Monospace v1.1/monospace/options/fields/button_set/field_button_set.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery('.buttonset').buttonset();
-});
-
-jQuery(document).ready(function(){
-	jQuery('.form-table').delegate(".ui-buttonset .ui-button", "click", function(event){
-        jQuery("html, body").animate({ scrollTop: jQuery(this).parent(".ui-buttonset").offset().top - 100 }, 600);
-	});
-});
