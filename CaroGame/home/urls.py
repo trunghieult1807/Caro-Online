da9544acff290adf975358a27de8ff0b57b0e30a
Thanh Nguyen <tienthanhnt123@gmail.com>
2020-06-28 17:01:26 +0700

    Change link address (urls) of homeview app

--- CaroGame/home/urls.py
+++ CaroGame/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('homepage', views.index, name='index'),
-	path('news', views.newsPage, name='news'),
-	path('play', views.playPage, name='play'),
+	path('home/', views.index, name='index'),
+	path('news/', views.newsPage, name='news'),
+	path('play/', views.playPage, name='play'),
 ]
