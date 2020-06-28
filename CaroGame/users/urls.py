516a32c6cac6e71e5f72606679e4c8cd941d3f08
Thanh Nguyen <tienthanhnt123@gmail.com>
2020-06-28 15:23:10 +0700

    Add homepage and user_login apps

---
+++ CaroGame/users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    #path('home/', views.home, name='users-home'),
+    # path('^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    #     views.activate, name='activate'),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
