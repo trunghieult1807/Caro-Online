a6e6f0a379e9e947fdf0dd5189aae7d75bf0e7bb
dirtyg2120 <zone2120vn@gmail.com>
2020-06-28 12:14:31 +0700

    remove home

--- Demo/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-	url('home', views.index, name='index'),
-	url('news', views.newsPage, name='news'),
-	url('play', views.playPage, name='play'),
-]
