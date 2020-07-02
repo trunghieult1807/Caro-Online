2b7a6d8910b2a53dc506d4791b65e39aa0b63a18
Thanh Nguyen <tienthanhnt123@gmail.com>
2020-07-02 18:50:40 +0700

    Add utils.py for database utilities

---
+++ CaroGame/caro/utils.py
@@ -0,0 +1,17 @@
+from caro.models import *
+from django.contrib.auth.models import User
+
+
+def clearSquares():
+    user = User.objects.get(pk=2)
+    GameSquare.objects.all().update(owner=None, status="Free")
+    Game.objects.filter(pk=6).update(current_turn=user, completed=None)
+    GameLog.objects.filter(game__id=6).delete()
+
+def clearRooms():
+    Room.objects.all().update(user1=None, user2=None, game=None)
+
+def clearAll():
+    Game.objects.all().delete()
+    GameCell.objects.all().delete()
+    GameLog.objects.all().delete()
