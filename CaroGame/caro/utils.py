ffe2f3efe4c8927799527850cf944cb257d76963
Thanh Nguyen <tienthanhnt123@gmail.com>
2020-07-04 20:17:13 +0700

    Change utils.py: add function to initilize database and clear them

--- CaroGame/caro/utils.py
+++ CaroGame/caro/utils.py
@@ -1,19 +1,39 @@
 from caro.models import *
 from django.contrib.auth.models import User
 
-def createRooms():
-    for i in range(10):
+number_of_games = 10
+number_of_rooms = 10
+
+def createGames(n=number_of_games):
+    for i in range(n):
+        game = Game.create_new()
+        game.save()
+
+def createRooms(n=number_of_rooms):
+    for i in range(n):
         room = Room(user1=None, user2=None, game=None)
         room.save()
 
-def clearSquares():
-    user = User.objects.get(pk=2)
-    GameSquare.objects.all().update(owner=None, status="Free")
-    Game.objects.filter(pk=6).update(current_turn=user, completed=None)
-    GameLog.objects.filter(game__id=6).delete()
+def initialize():
+    print(f"Create {number_of_games} ghost games...")
+    createGames()
+    print("Finish.")
 
-def clearAll():
+    print("Create 10 ghost rooms...")
+    createRooms()
+    print("Finish.")
+
+def clear():
     Room.objects.all().update(user1=None, user2=None, game=None)
+    Game.objects.all().update(creator=None, opponent=None, winner=None,
+                              current_turn=None, completed=False)
+    GameCell.objects.all().update(owner=None, status='EMPTY')
+
+def deleteGameLog():
+    GameLog.objects.all().delete()
+
+def delete():
+    Room.objects.all().delete()
     Game.objects.all().delete()
     GameCell.objects.all().delete()
     GameLog.objects.all().delete()
