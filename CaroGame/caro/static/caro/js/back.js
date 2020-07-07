f46710c57e80d885c78287a3f54b0613026efe47
Hieu Le <54405155+trunghieult1807@users.noreply.github.com>
2020-07-07 18:23:50 +0700

    Delete back.js

--- CaroGame/caro/static/caro/js/back.js
+++
@@ -1,4 +0,0 @@
-// Block back button
-window.location.hash="no-back-button";
-window.location.hash="Again-No-back-button";//again because google chrome don't insert first hash into history
-window.onhashchange=function(){window.location.hash="no-back-button";}
