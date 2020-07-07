ed343f1cc0fc02390d550e56c71e812363c4cacb
dirtyg2120 <zone2120vn@gmail.com>
2020-07-07 14:28:05 +0700

    update, HOME, USERS. Add media for avatar

--- CaroGame/home/views.py
+++ CaroGame/home/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from users.models import Profile
 
 # Create your views here.
-
 @login_required
 def index(request):
-	return render(request, 'pages/home.html')
+	# lists = Profile.objects.values('rank')
+	lists = list(Profile.objects.values_list('user', 'rank'))
+
+	for i in range(len(lists)):
+		lists[i] = list(lists[i])
+		user = User.objects.get(pk=lists[i][0])
+		lists[i][0] = user.username
+	
+	lists = sorted(lists,key=lambda l:l[1], reverse=True)
+	
+	return render(request, 'pages/home.html', {'lists': lists})
 
 @login_required
 def newsPage(request):
@@ -15,3 +26,9 @@ def newsPage(request):
 @login_required
 def playPage(request):
 	return render(request, 'pages/play.html')
+
+@login_required
+def profilePage(request):
+	return render(request, 'pages/profile.html')
+
+	
--- CaroGame/users/views.py
+++ CaroGame/users/views.py
@@ -11,9 +11,16 @@ from django.template.loader import render_to_string
 from .tokens import account_activation_token
 from django.contrib.auth.models import User
 from django.core.mail import EmailMessage
+from django.contrib.auth.decorators import login_required
 
+def home(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
+    return render(request, 'users/home.html')
 
-def register(request):
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
@@ -37,19 +44,26 @@ def register(request):
             form.save()
             messages.success(request, f'Account created successfully!')
             return redirect('login')
+        # else:
+        #     messages.warning(request, f'Error')
     else:
         form = RegistrationForm()
     return render(request, 'users/register.html', {'form': form})
 
-# def login(request):
-#     if request.method == 'POST':
-#         form = AuthenticationForm(request.POST)
-#         if form.is_valid():
-#             messages.success(request, f'Login successfully!')
-#             return render(request, 'users/home.html')
-#     else:
-#         form = AuthenticationForm()
-#     return render(request, 'users/login.html', {'form': form})
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    form = LoginForm(request.POST or None)
+    if request.POST and form.is_valid():
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+    return render(request, 'users/login.html', {'form': form })
 
 def activate(request, uidb64, token):
     try:
@@ -67,4 +81,6 @@ def activate(request, uidb64, token):
         return HttpResponse('Activation link is invalid!')
 
 def profile(request):
+    if not request.user.is_authenticated:
+        return redirect('login')
     return render(request, 'users/profile.html')
