cc81b6fa3e95ec58c562d78fcdc9d3a4bfeb2010
TruHee <trunghieult1807@gmail.com>
2020-07-30 22:03:33 +0700

    Add article page

--- caro_online/home/static/js/room_id.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".room-id").forEach(room => {
-	room.addEventListener('click', () => {
-		var roomId = room.name;
-		window.location.pathname = `/caro/${roomId}/`;
-	});
-});
--- caro_online/home/static/js/room_level.js
+++
@@ -1,8 +0,0 @@
-function seeRoomList() {
-	var x = document.getElementById("level");
-	if(x.style.display === "none") {
-		x.style.display = "block";
-	} else {
-		x.style.display = "none";
-	}
-}
